62d7557b6cd4b8739083725f41c10ab65af1f5f9
cozy cat <cozycat177@gmail.com>
2023-12-06 17:37:36 -0600

    add individual function for deposit, withdrawal and showbalance

--- src/App.java
+++ src/App.java
@@ -1,18 +1,40 @@
 import java.util.*;
 public class App {
+    // public static double balance = 10;
     public static void main(String[] args) throws Exception {
         // program allow user to make deposit, withdrawal or check balance
-        double balance = 0; 
         // double amount = 0;
         // init scanner object 
-        Scanner console = new Scanner(System.in);
+        double balance = 1000;
         boolean flag = true;
-        // ask user input 
+        // main loop ask user input 
         while (flag) {
+            Scanner console = new Scanner(System.in);
+            double amount = 0;
             printMenu();
             int choice = console.nextInt();
-            double res = choice(choice, console, balance);
-            System.out.println(res);
+            if (choice == 1) {
+                showBalance(balance);
+            } else if (choice == 2) {
+                System.out.println("enter amount to deposit: ");
+                amount = console.nextDouble();
+                if(deposit(amount, balance) != 0) {
+                    balance = deposit(amount, balance);
+                } else {
+                    System.out.println("something went wrong...");
+                }
+            } else if (choice == 3) {
+                System.out.println("enter amount to withdrwal: ");
+                amount = console.nextDouble();
+                if (withdrawal(balance, amount) != 0) {
+                    balance = withdrawal(balance, amount);
+                } else {
+                    System.out.println("something went wrong...");
+                }
+            } else if (choice == 4) {
+                System.out.println("program end");
+                flag = false;
+            }
         }
     }
 
@@ -26,41 +48,24 @@ public class App {
         System.out.print("Enter a number to continue: ");        
     }
 
-    // take the choice switch statement into a function.
-    static double choice(int choice, Scanner console, double balance) {
-        double amount = 0;
-        switch(choice) {
-                case 1:
-                    // System.out.println("current balance: " + balance + "\n");
-                    return balance;
-                case 2:
-                    System.out.println("Enter amount to deposit: \n");
-                    amount = console.nextDouble();
-                    if (amount > 0) {
-                        return balance += amount;
-                    } else {
-                        // System.out.println("deposit amount must be greater than 0");
-                        return -2.0;
-                    }
-                    // break;
-                case 3:
-                    System.out.println("Enter amount to withdrawal: \n");
-                    amount = console.nextDouble();
-                    if (balance > amount && balance > 0) {
-                        return balance -= amount;
-                    } else {
-                        // System.out.println("withdrwal amount must be less than balance and greater than 0");
-                        return -3;
-                    }
-                    // break;
-                case 4:
-                    System.out.println("program end");
-                    console.close();
-                    return -4;
-                    // flag = -1;
-                default:
-                    return 404;
-            }
+    // add deposit, withdrawal and showBalance functions
+    static void showBalance(double balance) {
+        System.out.println(balance);
+    }
 
+    static double deposit(double amount, double balance) {
+        if (amount > 0) {
+            return balance += amount;
+        } else {
+            return 0;
+        }
+    }
+
+    static double withdrawal(double balance, double amount) {
+        if (balance > amount && amount > 0) {
+            return balance -= amount;
+        } else {
+            return 0;
+        }
     }
 }
