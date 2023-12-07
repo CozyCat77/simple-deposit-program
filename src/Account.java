1e2c6840ed8fa2151e1d9df6a5e78d1972fe0e12
cozy cat <cozycat177@gmail.com>
2023-12-07 16:58:31 -0600

    added Account.java file

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+public class Account {
+    // attributes for deposit account
+    // username
+    // balance
+    // password
+    // contact
+    String username = "Lisa";
+    double balance = 10000;
+    String password = "787877lisa";
+    String contact = "lisaluv@hotmail.com";
+
+    void showBalance() {
+        System.out.println("current balance: " + balance);
+    }
+
+    void showContact() {
+        System.out.println("contact info: " + contact);
+    }
+}
